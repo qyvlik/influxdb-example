a49b8397dbedd65afcbe3bb0aa2b8a3b80d83c28
qyvlik <qyvlik@qq.com>
2019-08-19 16:57:27 +0800

    add code

---
+++ src/main/java/io/github/qyvlik/influxdbexample/InfluxdbExampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.qyvlik.influxdbexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluxdbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfluxdbExampleApplication.class, args);
+	}
+
+}
